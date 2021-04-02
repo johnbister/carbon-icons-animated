e292697da868b2dab4d7437c812acb8bdad7eda4
johnbister <john.bister@ibm.com>
2021-04-02 12:09:51 -0700

    add page last icon

---
+++ src/components/IconAnimations/PageLastIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg class="icon icon--pagelast" aria-hidden="true" viewBox="0 -10 36 44">
+    <title>page last</title>
+    <path class="page-last-arrow" d="M18,16L8,26l-1.4-1.4l8.6-8.6L6.6,7.4L8,6L18,16z" />
+    <path class="page-last-line" d="M22,4h2v24h-2V4z" />
+  </svg>
+</template>
+<script>
+export default {
+  name: "PageLastIcon"
+};
+</script>
+<style lang="scss">
+.icon--pagelast {
+  width: 36px;
+  height: 44px;
+  margin-top: -12px;
+  overflow: visible
+}
+</style>
