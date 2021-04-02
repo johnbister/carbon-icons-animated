e2b8e2e893bf3d13792b5d00e8c42aae15aee916
johnbister <john.bister@ibm.com>
2021-04-02 12:10:11 -0700

    add up to top icon

---
+++ src/components/IconAnimations/UpToTopIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg class="icon icon--uptotop" aria-hidden="true" viewBox="0 -10 36 44">
+    <title>up to top</title>
+    <path class="up-to-top-arrow" d="M16,14L6,24l1.4,1.4l8.6-8.6l8.6,8.6L26,24L16,14z" />
+    <path class="up-to-top-line" d="M4,8h24v2H4V8z" />
+  </svg>
+</template>
+<script>
+export default {
+  name: "UpToTopIcon"
+};
+</script>
+<style lang="scss">
+.icon--uptotop {
+  width: 36px;
+  height: 44px;
+  margin-top: -12px;
+  overflow: visible
+}
+</style>
