c129771f0e9bac2a250ab0c0fbbaab779e7bf106
johnbister <john.bister@ibm.com>
2021-04-02 12:10:04 -0700

    add send icon

---
+++ src/components/IconAnimations/SendIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg class="icon icon--chat" aria-hidden="true" viewBox="0 -10 36 44">
+    <title>contact us</title>
+    <path class="send-line1" d="M0,13h8.9v2H0V13z" />
+    <path class="send-line2" d="M5,17h6.1v2H5V17z" />
+    <path class="send" d="M27.9,15.5c-0.1-0.2-0.2-0.3-0.4-0.4l-22-11C5.1,3.9,4.7,4,4.4,4.2C4.1,4.5,4,4.9,4,5.2L7,16L4,26.7
+		c-0.1,0.5,0.2,1.1,0.7,1.2c0.1,0,0.2,0,0.3,0c0.2,0,0.3,0,0.5-0.1l22-11C27.9,16.6,28.1,16,27.9,15.5z M6.5,25.1L8.8,17H18v-2H8.8
+		L6.5,6.9L24.8,16L6.5,25.1z"/>
+  </svg>
+</template>
+<script>
+export default {
+  name: "ChatIcon"
+};
+</script>
+<style lang="scss">
+.icon--chat {
+  width: 36px;
+  height: 44px;
+  margin-top: -12px;
+  overflow: visible
+}
+</style>
