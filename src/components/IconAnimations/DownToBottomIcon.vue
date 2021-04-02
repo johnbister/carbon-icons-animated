3f411bffcc01b31bb836993351b48be992b94ba0
johnbister <john.bister@ibm.com>
2021-04-02 12:09:14 -0700

    add down to bottom icon

---
+++ src/components/IconAnimations/DownToBottomIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg class="icon icon--downtobottom" aria-hidden="true" viewBox="0 -10 36 44">
+    <title>down to bottom</title>
+    <path class="down-to-bottom-arrow" d="M16,18L6,8l1.4-1.4l8.6,8.6l8.6-8.6L26,8L16,18z" />
+    <path class="down-to-bottom-line" d="M4,22h24v2H4V22z" />
+  </svg>
+</template>
+<script>
+export default {
+  name: "DownToBottomIcon"
+};
+</script>
+<style lang="scss">
+.icon--downtobottom {
+  width: 36px;
+  height: 44px;
+  margin-top: -12px;
+  overflow: visible
+}
+</style>
