80697a5cbc02d763e0b7602eab4d7f9c1ecd656e
johnbister <john.bister@ibm.com>
2021-04-29 11:37:52 -0700

    add up to top icon

--- src/components/IconAnimations/UpToTopIcon.vue
+++ src/components/IconAnimations/UpToTopIcon.vue
@@ -1,7 +1,10 @@
 <template>
   <svg class="icon icon--uptotop" aria-hidden="true" viewBox="0 -10 36 44">
     <title>up to top</title>
-    <path class="up-to-top-arrow" d="M16,14L6,24l1.4,1.4l8.6-8.6l8.6,8.6L26,24L16,14z" />
+    <path
+      class="up-to-top-arrow"
+      d="M16,14L6,24l1.4,1.4l8.6-8.6l8.6,8.6L26,24L16,14z"
+    />
     <path class="up-to-top-line" d="M4,8h24v2H4V8z" />
   </svg>
 </template>
@@ -15,6 +18,6 @@ export default {
   width: 36px;
   height: 44px;
   margin-top: -12px;
-  overflow: visible
+  overflow: visible;
 }
 </style>
