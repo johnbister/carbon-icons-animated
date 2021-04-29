7c2f6369ca35958a2082ac09495acc7fb1b4bd8f
johnbister <john.bister@ibm.com>
2021-04-29 11:38:03 -0700

    add volume up icon

---
+++ src/components/IconAnimations/VolumeUpIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg class="icon--volume-up" aria-hidden="true">
+    <title>Volume up</title>
+      <path class="volumeupspeaker" d="M18,30c-0.3,0-0.5-0.1-0.7-0.3L9.7,22H3c-0.6,0-1-0.4-1-1l0,0V11c0-0.6,0.4-1,1-1h6.7l7.6-7.7c0.4-0.4,1-0.4,1.4,0
+        C18.9,2.5,19,2.7,19,3v26C19,29.6,18.6,30,18,30z M4,20h6.1c0.3,0,0.5,0.1,0.7,0.3l6.2,6.3V5.4l-6.2,6.3c-0.2,0.2-0.4,0.3-0.7,0.3
+        H4V20z"/>
+      <path class="volume-2" d="M27.2,8.1l-1.5,1.3c3.3,3.9,3.1,9.5-0.3,13.2l1.5,1.4C30.9,19.6,31.1,12.7,27.2,8.1L27.2,8.1z"/>
+      <path class="volume-1" d="M21.6,12c2,2.3,1.9,5.7-0.2,7.9l1.5,1.4c2.7-3,2.8-7.5,0.2-10.6L21.6,12z"/>
+  </svg>
+</template>
+<script>
+export default {
+  name: "VolumeUpIcon"
+};
+</script>
+<style lang="scss">
+.icon--volume-up {
+  width: 32px;
+  height: 32px;
+}
+</style>
