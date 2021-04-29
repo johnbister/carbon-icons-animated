261429bdc81e07034f6c791924ba60406ea77940
johnbister <john.bister@ibm.com>
2021-04-29 11:36:24 -0700

    add page first icon

--- src/components/IconAnimations/PageFirstIcon.vue
+++ src/components/IconAnimations/PageFirstIcon.vue
@@ -1,7 +1,10 @@
 <template>
   <svg class="icon icon--pagefirst" aria-hidden="true" viewBox="0 -10 36 44">
     <title>page first</title>
-    <path class="page-first-arrow" d="M14,16L24,6l1.4,1.4L16.8,16l8.6,8.6L24,26L14,16z" />
+    <path
+      class="page-first-arrow"
+      d="M14,16L24,6l1.4,1.4L16.8,16l8.6,8.6L24,26L14,16z"
+    />
     <path class="page-first-line" d="M8,4h2v24H8V4z" />
   </svg>
 </template>
@@ -15,6 +18,6 @@ export default {
   width: 36px;
   height: 44px;
   margin-top: -12px;
-  overflow: visible
+  overflow: visible;
 }
 </style>
