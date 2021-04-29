20f9b680dc64cd6ffb156aac8010770cc1969c4a
johnbister <john.bister@ibm.com>
2021-04-29 11:34:00 -0700

    add chat icon

--- src/components/IconAnimations/ChatIcon.vue
+++ src/components/IconAnimations/ChatIcon.vue
@@ -1,16 +1,13 @@
 <template>
   <svg class="icon icon--chat" aria-hidden="true" viewBox="0 -10 36 44">
     <title>contact us</title>
+    <path class="text1" d="M8,10h16v2H8V10z" />
+    <path class="text2" d="M8,16h10v2H8V16z" />
     <path
-      class="text1"
-      d="M8,10h16v2H8V10z" 
+      class="bubble"
+      d="M17.7,30L16,29l4-7h6c1.1,0,2-0.9,2-2V8c0-1.1-0.9-2-2-2H6C4.9,6,4,6.9,4,8v12c0,1.1,0.9,2,2,2h9v2H6c-2.2,0-4-1.8-4-4V8
+		c0-2.2,1.8-4,4-4h20c2.2,0,4,1.8,4,4v12c0,2.2-1.8,4-4,4h-4.8L17.7,30z"
     />
-    <path
-      class="text2"
-      d="M8,16h10v2H8V16z" 
-    />
-    <path class="bubble" d="M17.7,30L16,29l4-7h6c1.1,0,2-0.9,2-2V8c0-1.1-0.9-2-2-2H6C4.9,6,4,6.9,4,8v12c0,1.1,0.9,2,2,2h9v2H6c-2.2,0-4-1.8-4-4V8
-		c0-2.2,1.8-4,4-4h20c2.2,0,4,1.8,4,4v12c0,2.2-1.8,4-4,4h-4.8L17.7,30z" />
   </svg>
 </template>
 <script>
