c8db64f15b7e69cd86796847e3ea4efbcea06d7f
johnbister <john.bister@ibm.com>
2021-04-29 11:36:18 -0700

    add overflow icon

---
+++ src/components/IconAnimations/OverflowMenuHorizontalIcon.vue
@@ -0,0 +1,25 @@
+<template>
+<svg class="icon--overflow-menu--horizontal" width="32px" height="32px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+	<title>overflow-menu--horizontal</title>
+	<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+		<g id="overflow-menu--horizontal-original">
+			<circle class="circle-left" fill="#000000" fill-rule="nonzero" cx="8" cy="16" r="2"></circle>
+			<circle class="circle-middle" fill="#000000" fill-rule="nonzero" cx="16" cy="16" r="2"></circle>
+			<circle class="circle-right" fill="#000000" fill-rule="nonzero" cx="24" cy="16" r="2"></circle>
+			<rect id="_Transparent_Rectangle_" x="0" y="0" width="32" height="32"></rect>
+		</g>
+	</g>
+</svg>
+</template>
+<script>
+export default {
+  name: "OverflowMenuHorizontalIcon"
+}
+</script>
+<style lang="scss">
+.icon--overflow-menu-horizontal {
+  width: 32px;
+  height: 32px;
+  transition: 0.2s all;
+}
+</style>
