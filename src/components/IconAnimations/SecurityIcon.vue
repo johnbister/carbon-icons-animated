55d2f1f9ac52989cf48ec5bb009bfb93e1910926
johnbister <john.bister@ibm.com>
2021-04-29 11:37:04 -0700

    add security icon

---
+++ src/components/IconAnimations/SecurityIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg class="icon--security" aria-hidden="true">
+    <title>security</title>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="security">
+            <g id="shield" transform="translate(4.000000, 2.000000)" fill="#000000" fill-rule="nonzero">
+                <path d="M12,28 L5.8,24.7 C2.2,22.8 0,19.1 0,15 L0,2 C0,0.9 0.9,0 2,0 L22,0 C23.1,0 24,0.9 24,2 L24,15 C24,19.1 21.8,22.8 18.2,24.7 L12,28 Z M2,2 L2,15 C2,18.3 3.8,21.4 6.8,22.9 L12,25.7 L17.2,22.9 C20.1,21.3 22,18.3 22,15 L22,2 L2,2 Z" id="Shape"></path>
+            </g>
+            <g id="transparent">
+                <rect id="_Transparent_Rectangle_" x="0" y="0" width="32" height="32"></rect>
+            </g>
+            <g class="security-checkmark" transform="translate(10.000000, 10.000000)" stroke="#000000" stroke-width="2">
+                <polyline id="Path" points="0.7 4.7 4 8 11.3 0.7"></polyline>
+            </g>
+        </g>
+    </g>
+  </svg>
+</template>
+<script>
+export default {
+  name: "SecurityIcon"
+};
+</script>
+<style lang="scss">
+.icon--security {
+  width: 32px;
+  height: 32px;
+}
+</style>
