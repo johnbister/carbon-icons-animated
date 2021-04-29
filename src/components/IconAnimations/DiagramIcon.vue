fcfa29c5f81063109d4ca5fd478b7f90a82f6001
johnbister <john.bister@ibm.com>
2021-04-29 11:35:15 -0700

    add diagram icon

---
+++ src/components/IconAnimations/DiagramIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg class="icon--diagram" aria-hidden="true">
+    <title>diagram</title>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g id="diagram">
+        <path class="diagram-chart" d="M13,30 C6.92486775,30 2,25.0751322 2,19 C2,12.9248678 6.92486775,8 13,8 C13.5522847,8 14,8.44771525 14,9 L14,18 L23,18 C23.5522847,18 24,18.4477153 24,19 C24,25.0751322 19.0751322,30 13,30 Z M12,10.06 C7.31602542,10.5811351 3.82730175,14.6274812 4.00132983,19.337143 C4.17535792,24.0468047 7.95319526,27.8246421 12.662857,27.9986702 C17.3725188,28.1726982 21.4188649,24.6839746 21.94,20 L14,20 C12.8954305,20 12,19.1045695 12,18 L12,10.06 Z"  fill="#000000" fill-rule="nonzero"></path>
+        <path class="diagram-slice" d="M28,14 L19,14 C17.8954305,14 17,13.1045695 17,12 L17,3 C17,2.44771525 17.4477153,2 18,2 C24.0751322,2 29,6.92486775 29,13 C29,13.5522847 28.5522847,14 28,14 Z M19,12 L26.94,12 C26.4713318,7.82434654 23.1756535,4.52866825 19,4.06 L19,12 Z"  fill="#000000" fill-rule="nonzero"></path>
+        <rect id="_Transparent_Rectangle_" x="0" y="0" width="32" height="32"></rect>
+      </g>
+    </g>
+  </svg>
+</template>
+<script>
+export default {
+  name: "DiagramIcon"
+};
+</script>
+<style lang="scss">
+.icon--diagram {
+  width: 32px;
+  height: 32px;
+}
+</style>
