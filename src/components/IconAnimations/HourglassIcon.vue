9c56bffe7cf3b23303ee67de4cc27f7868386b3c
johnbister <john.bister@ibm.com>
2021-05-10 09:00:22 -0700

    add hourglass icon

---
+++ src/components/IconAnimations/HourglassIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg class="icon--hourglass" aria-hidden="true">
+    <title>hourglass</title>
+    <line class="sand" x1="16" y1="19" x2="16" y2="25" stroke="#000000" stroke-width="2" stroke-miterlimit="10" stroke-dasharray="2,2"/>
+    <path class="hourglass"  d="M23,11.7V4h3V2H6v2h3v7.7c0,0.4,0.1,0.9,0.4,1.2l2.4,3.1l-2.4,3.1C9.1,19.5,9,19.9,9,20.3V28H6v2h20v-2h-3v-7.7
+		c0-0.4-0.1-0.9-0.4-1.2L20.2,16l2.4-3.1C22.9,12.5,23,12.1,23,11.7z M21,4v7H11V4H21z M21,20.3V28H11v-7.7l3.2-4.3L12,13h8l-2.2,3
+		L21,20.3z"/>
+    <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" fill="none" width="32" height="32"/>
+  </svg>
+</template>
+<script>
+export default {
+  name: "HourglassIcon"
+};
+</script>
+<style lang="scss">
+.icon--hourglass {
+  width: 32px;
+  height: 32px;
+}
+</style>
