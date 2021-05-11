1fb355e536d7444d2f0e0b01eab60c8ffa8a1564
johnbister <john.bister@ibm.com>
2021-05-11 07:48:03 -0700

    add timer icon

---
+++ src/components/IconAnimations/TimerIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg class="icon--timer" aria-hidden="true">
+    <title>timer</title>
+    <path class="timer" d="M28,9l-1.4-1.4l-2.2,2.2c-4-4.6-10.9-5.1-15.4-1.1S3.8,19.6,7.8,24.2s10.9,5.1,15.4,1.1c4-3.4,4.9-9.2,2.3-13.8L28,9z
+		 M16,26c-5,0-9-4-9-9s4-9,9-9s9,4,9,9S21,26,16,26z"/>
+    <rect class="timer-hand" x="15" y="11" width="2" height="9"/>
+    <rect class="timer-start" x="13" y="2" width="6" height="2"/>
+    <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" fill="none" width="32" height="32"/>
+  </svg>
+</template>
+<script>
+export default {
+  name: "TimerIcon"
+};
+</script>
+<style lang="scss">
+.icon--timer {
+  width: 32px;
+  height: 32px;
+}
+</style>
