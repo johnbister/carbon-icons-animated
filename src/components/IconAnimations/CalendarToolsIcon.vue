b40531e22091185b00191d3c1141bec0ad53d318
johnbister <john.bister@ibm.com>
2021-05-13 08:48:23 -0700

    add calendar tools icon

---
+++ src/components/IconAnimations/CalendarToolsIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg class="icon--calendar-tools" aria-hidden="true" viewBox="0 0 32 32" width="32" height="32" >
+    <title>calendar tools</title>
+    <path class="calendar-tools" d="M26,4h-4V2h-2v2h-8V2h-2v2H6C4.9,4,4,4.9,4,6v20c0,1.1,0.9,2,2,2h8v-2H6V6h4v2h2V6h8v2h2V6h4v8h2V6
+		C28,4.9,27.1,4,26,4z"/>
+    <path class="calendar-tools-wrench" d="M24,24.4c0.6,0.4,1.3,0.6,2,0.6c2.2,0,4-1.8,4-4c0-0.3-0.1-0.7-0.1-1l-2.4,2.4l0,0c-0.8,0.8-2,0.8-2.8,0s-0.8-2,0-2.8l0,0
+		l0,0l2.4-2.4c-0.3-0.1-0.7-0.1-1-0.1c-2.2,0-4,1.8-4,4c0,0.7,0.2,1.4,0.6,2L17,28.6l1.4,1.4L24,24.4z"/>
+    <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" fill="none" width="32" height="32"/>
+  </svg>
+</template>
+<script>
+export default {
+  name: "CalendarToolsIcon"
+};
+</script>
+<style lang="scss">
+.icon--calendar-tools-icon {
+  width: 32px;
+  height: 32px;
+}
+</style>
