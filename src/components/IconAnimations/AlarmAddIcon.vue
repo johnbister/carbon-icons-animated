47280cff0a60ad737029acd6ba0e741332f4ae2f
johnbister <john.bister@ibm.com>
2021-05-13 08:47:49 -0700

    add alarm add icon

--- src/components/IconAnimations/AlarmAddIcon.vue
+++ src/components/IconAnimations/AlarmAddIcon.vue
@@ -1,10 +1,10 @@
 <template>
   <svg class="icon--alarm-add" aria-hidden="true" viewBox="0 0 32 32" width="32" height="32" >
     <title>alarm add</title>
-    <path class="alarm-clock" d="M16,28C9.9,28,5,23.1,5,17S9.9,6,16,6s11,4.9,11,11S22.1,28,16,28z M16,8c-5,0-9,4-9,9s4,9,9,9s9-4,9-9S21,8,16,8z"/>
-    <path class="alarm-plus" d="M21,16h-4v-4h-2v4h-4v2h4v4h2v-4h4V16z"/>
-    <path class="alarm-bell1" d="M4,7.6L7.6,4L9,5.4L5.4,9L4,7.6z"/>
-    <path class="alarm-bell2" d="M23,5.4L24.4,4L28,7.6L26.6,9L23,5.4z"/>
+    <path class="alarm-add-clock" d="M16,28C9.9,28,5,23.1,5,17S9.9,6,16,6s11,4.9,11,11S22.1,28,16,28z M16,8c-5,0-9,4-9,9s4,9,9,9s9-4,9-9S21,8,16,8z"/>
+    <path class="alarm-add-plus" d="M21,16h-4v-4h-2v4h-4v2h4v4h2v-4h4V16z"/>
+    <path class="alarm-add-bell1" d="M4,7.6L7.6,4L9,5.4L5.4,9L4,7.6z"/>
+    <path class="alarm-add-bell2" d="M23,5.4L24.4,4L28,7.6L26.6,9L23,5.4z"/>
     <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" fill="none" width="32" height="32"/>
   </svg>
 </template>
@@ -14,7 +14,7 @@ export default {
 };
 </script>
 <style lang="scss">
-.icon--alarm-icon {
+.icon--alarm-add-icon {
   width: 32px;
   height: 32px;
 }
