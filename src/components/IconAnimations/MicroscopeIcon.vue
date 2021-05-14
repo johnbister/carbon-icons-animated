40071d42c003c8e1a7086e47d63dd0ed51a61431
johnbister <john.bister@ibm.com>
2021-05-14 09:58:16 -0700

    add microscope svg

---
+++ src/components/IconAnimations/MicroscopeIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg class="icon--microscope" aria-hidden="true" viewBox="0 0 32 32" width="32" height="32" >
+    <title>microscope</title>
+    <path class="microscope-head" d="M23.7,15.4c0.6-1.6,0.2-3.3-1.1-4.4l2.8-2.8c0.8-0.8,0.8-2,0-2.8l-2.8-2.8c-0.8-0.8-2-0.8-2.8,0L6.6,15.8
+			c-0.8,0.8-0.8,2,0,2.8l2.8,2.8c0.8,0.8,2,0.8,2.8,0l4.8-4.8c1.4,1.6,3.9,1.8,5.5,0.4C23.4,16.4,23.7,15.4,23.7,15.4z M10.8,20
+			L8,17.2 M16,14c0,0.3,0,0.5,0.1,0.8L10.8,20L8,17.2L21.2,4L24,6.8L20.8,10c-2.2-0.4-4.2,1-4.7,3.1C16,13.5,16,13.7,16,14z M20,16
+			c-1.1,0-2-0.9-2-2s0.9-2,2-2s2,0.9,2,2S21.1,16,20,16z"/>
+    <path class="microscope-base" d="M25.4,24c1.2-2.9,0.6-6.3-1.7-8.6C22.8,16,22.5,17,22.5,17c1.7,1.9,1.9,4.7,0.6,6.9H16v4H4v2h24v-6h-2.6V24z M26,28h-8v-2
+			h8V28z"/>
+    <line class="microscope-adjust" x1="9.9" y1="13.9" x2="14" y2="18.1" stroke="#000000" stroke-width="2"/>
+    <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" fill="none" width="32" height="32"/>
+  </svg>
+</template>
+<script>
+export default {
+  name: "MicroscopeIcon"
+};
+</script>
+<style lang="scss">
+.icon--microscope-icon {
+  width: 32px;
+  height: 32px;
+}
+</style>
