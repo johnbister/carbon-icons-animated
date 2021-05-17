c2c00493d491223c3b12c8505173237b4a9e88d4
johnbister <john.bister@ibm.com>
2021-05-17 09:27:10 -0700

    add stamp icon svg

---
+++ src/components/IconAnimations/StampIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg class="icon--stamp" aria-hidden="true" viewBox="0 0 32 32" width="32" height="32" >
+    <title>stamp</title>
+    <path class="stamp-rubber" d="M6,26v2c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2v-2C27.1,26,4.9,26,6,26z M24,28H8v-2h16V28z"/>
+    <path class="stamp-base" d="M26,18h-6v-3.1c0-1.4,0.4-2.7,1.2-3.9c1.5-2.6,0.9-5.8-1.4-7.7c-2.6-2.1-6.3-1.7-8.4,0.9c-0.6,0.8-1.1,1.7-1.2,2.7
+		c-0.3,1.4,0,2.9,0.7,4.2c0.7,1.1,1.1,2.4,1.1,3.7V18H6c-1.1,0-2,0.9-2,2v4c0,1.1,0.9,2,2,2h20c1.1,0,2-0.9,2-2v-4
+		C28,18.9,27.1,18,26,18z M12.1,7.2c0.4-2.2,2.5-3.6,4.7-3.2s3.6,2.5,3.2,4.7c-0.1,0.5-0.3,0.9-0.5,1.3c-0.7,1.2-1.2,2.6-1.4,4h-4.1
+		c-0.2-1.4-0.6-2.8-1.4-4C12.1,9.2,11.9,8.2,12.1,7.2z M18,16v2h-4v-2H18z M6,24v-4h20v4H6z"/>
+    <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" fill="none" width="32" height="32"/>
+  </svg>
+</template>
+<script>
+export default {
+  name: "StampIcon"
+};
+</script>
+<style lang="scss">
+.icon--stamp-icon {
+  width: 32px;
+  height: 32px;
+}
+</style>
