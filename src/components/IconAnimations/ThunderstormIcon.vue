186c95361a2f0520b00ad85dcafa32e6a43ecc87
johnbister <john.bister@ibm.com>
2021-05-20 08:16:51 -0700

    add thunderstorm icon svg

---
+++ src/components/IconAnimations/ThunderstormIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg class="icon--thunderstorm" aria-hidden="true" viewBox="0 0 32 32" width="32" height="32" >
+    <title>thunderstorm</title>
+    <path class="thunderstorm-cloud" d="M23.5,22H23v-2h0.5c2.5,0.1,4.6-1.8,4.7-4.3s-1.8-4.6-4.3-4.7H23l-0.1-0.8c-0.5-3.8-4-6.5-7.9-6c-3.1,0.4-5.6,2.9-6,6L9,11
+		H8.1c-2.5,0.1-4.4,2.2-4.3,4.7C3.9,18.2,6,20.1,8.5,20H9v2H8.5C4.9,22,2,19,2,15.5c0-3.1,2.2-5.7,5.2-6.3c1-4.9,5.8-8,10.7-6.9
+		c3.5,0.7,6.2,3.4,6.9,6.9c3.5,0.7,5.8,4.2,5,7.7C29.2,19.8,26.6,22,23.5,22z"/>
+    <polyline class="thunderstorm-bolt" stroke="#000000" stroke-width="2" fill=none points="17,16 13,23 19.1,23 15,30 	"/>
+    <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" fill="none" width="32" height="32"/>
+  </svg>
+</template>
+<script>
+export default {
+  name: "AlarmAddIcon"
+};
+</script>
+<style lang="scss">
+.icon--alarm-icon {
+  width: 32px;
+  height: 32px;
+}
+</style>
